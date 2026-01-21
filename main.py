5afd9a43e6b66e975cddb971ccd45f0355c2e50f
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2026-01-21 22:19:42 +0600

    added home and post route and ran the application

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 from fastapi import FastAPI
 app = FastAPI()
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025"
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI.",
+        "date_posted": "April 21, 2025"
+    }
+]
+
+@app.get("/")
+def home():
+    return {"message": "Hello, World everyone!"}
+
+@app.get("/posts")
+def get_posts():
+    return posts
