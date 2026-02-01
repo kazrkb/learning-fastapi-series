33ccec9821c8c05759002c264aad84c020b2e3b4
Kazi Md. Rakibul Hassan <kazrkb@gmail.com>
2026-02-01 12:41:56 +0600

    created a layout format and extended it to home html

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ posts: list[dict] = [
 @app.get("/", include_in_schema=False)
 @app.get("/posts", include_in_schema=False)
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html", {"posts": posts})
+    return templates.TemplateResponse(request, "home.html", {"posts": posts,"title": "Home"})
 
 @app.get("/api/posts")
 def get_posts():
